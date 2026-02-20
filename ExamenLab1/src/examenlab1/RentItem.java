bfb2cdb1d257c94a80e4c6a47cd5cc9ad7b23490
Marco Vásquez <marco_vasquez1@unitec.edu>
2026-02-20 08:45:05 -0600

    Clase RentItem buena

--- ExamenLab1/src/examenlab1/RentItem.java
+++ ExamenLab1/src/examenlab1/RentItem.java
@@ -5,10 +5,47 @@
  */
 package examenlab1;
 
+import javax.swing.ImageIcon;
+
 /**
  *
  * @author andres
  */
-public class RentItem {
-    
+public abstract class RentItem {
+    private String codigo;
+    private String nombreItem;
+    private double baseRenta;
+    private int copiasDisponibles;
+    private ImageIcon imagen;
+    private RentItem(String codigo, String nombreItem, double baseRenta){
+        this.codigo=codigo;
+        this.nombreItem=nombreItem;
+        this.baseRenta=baseRenta;
+        this.copiasDisponibles=0;
+    }
+    public abstract double pagoRenta(int dias);
+    public String getCodigo(){
+        return codigo;
+    }
+    public String getNombreItem(){
+        return nombreItem;
+    }
+    public double getBaseRenta(){
+        return baseRenta;
+    }
+    public int getCopiasDisponibles(){
+        return copiasDisponibles;
+    }
+    public void setCopiasDisponibles(int copias){
+        this.copiasDisponibles=copias;
+    }
+    public ImageIcon getImagen(){
+        return imagen;
+    }
+    public void setImagen(ImageIcon imagen){
+        this.imagen=imagen;
+    }
+    public String toString(){
+        return "Código: "+codigo+" | Nombre: "+nombreItem+" | Precio Base: "+baseRenta+" Lps. | Copias: "+copiasDisponibles;
+    }
 }
